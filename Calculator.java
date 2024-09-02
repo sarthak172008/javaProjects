5661b65992ef174d39f89639fb32f26393113425
Sarthak172008 <sarthaktamrakar172008@gmail.com>
2024-09-02 17:05:33 +0530

    Update Calculator.java
    Commit changes

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,6 @@
 import java.util.*;
 
-public static void main(String args[]) {
-    Scanner sc = new Scanner(System.in);
-    int func = sc.nextInt();
-    double a = sc.nextInt();
-    double b = sc.nextInt();
-
+public static void calculator(int func, double a, double b) {
     switch(func) {
         case 1: System.out.println(a+b);
         break;
@@ -19,4 +14,16 @@ public static void main(String args[]) {
         break;
         default: System.out.println("Wrong path");
     }
+}
+public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Select calculation path");
+    System.out.println("|1 for Addition| |2 for Subtraction| |3 for Multiplaction| |4 for Divition| |5 for Modulo|");
+    int func = sc.nextInt();
+    System.out.println("Enter first number");
+    double a = sc.nextInt();
+    System.out.println("Enter second number");
+    double b = sc.nextInt();
+
+    calculator(func, a, b);
 }
