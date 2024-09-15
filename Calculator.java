032aeba073fd1ca5f910d5396f7d9671e4b86cc8
Sarthak172008 <sarthaktamrakar172008@gmail.com>
2024-09-15 11:49:13 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
-public static void calculator(int func, double a, double b) {
+public static void calculator(int method, double a, double b) {
     System.out.print("Your answer is : ");
-    switch(func) {
+    switch(method) {
         case 1: System.out.println(a+b);
         break;
         case 2: System.out.println(a-b);
@@ -20,7 +20,7 @@ public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Select calculation path");
     System.out.println("|1 for Addition| |2 for Subtraction| |3 for Multiplaction| |4 for Divition| |5 for Modulo|");
-    int func = sc.nextInt();
+    int method = sc.nextInt();
     System.out.println("Enter first number");
     double a = sc.nextInt();
     System.out.println("Enter second number");
