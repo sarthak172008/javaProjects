d1cbff8713ef0c64041c8b037aef33ffc927f52c
Sarthak172008 <sarthaktamrakar172008@gmail.com>
2024-11-02 22:16:18 +0530

    Update Calculator.java
    More better

--- Calculator.java
+++ Calculator.java
@@ -1,40 +1,34 @@
 import java.util.*;
 
-public class Calculator{
-    public static void calculator(int method, double a, double b) {
-        System.out.print("Your answer is : ");
-        switch(method) {
-            case 1: System.out.println(a+b);
+public class Calculator {
+    public static void calculator(int path, double a, double b) {
+        switch(path) {
+            case 1: System.out.println("your answer is : "+(a+b));
             break;
-            case 2: System.out.println(a-b);
+            case 2: System.out.println("your answer is : "+(a-b));
             break;
-            case 3: System.out.println(a*b);
+            case 3: System.out.println("your answer is : "+(a*b));
             break;
-            case 4: if(b==0) {
-                System.out.println("invalid Division");
-                } else {
-                System.out.println(a/b);
-            }
+            case 4: System.out.println("your answer is : "+(a/b));
             break;
             case 5: if(b==0) {
-                System.out.println("Invalid Division");
-                } else {
-                System.out.println(a%b);
+                System.out.println("Invalid Calculation");
+            } else {
+                System.out.println("your answer is : "+(a%b));
             }
             break;
-            default: System.out.println("Wrong path");
+            default: System.out.println("Invalid Path");
         }
     }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Select calculation path");
-        System.out.println("|1 for Addition| |2 for Subtraction| |3 for Multiplaction| |4 for Divition| |5 for Modulo|");
-        int method = sc.nextInt();
-        System.out.println("Enter first number");
+        System.out.println("Enter 1 for Addition || Enter 2 for Subtration || Enter 3 for Multiplaction || Enter 4 Divition || Enter 5 for Modulo");
+        int path = sc.nextInt();
+        System.out.print("Enter your first Number : ");
         double a = sc.nextDouble();
-        System.out.println("Enter second number");
+        System.out.print("Enter your second Number : ");
         double b = sc.nextDouble();
 
-        calculator(method, a, b);
+        calculator(path, a, b);
     }
 }
