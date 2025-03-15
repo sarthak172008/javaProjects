efd0d68583d76159416c54b72e888a82975c401a
Sarthak172008 <sarthaktamrakar172008@gmail.com>
2025-03-15 17:18:52 +0530

    Updated

---
+++ Calculator/Calculation.java
@@ -0,0 +1,24 @@
+package Calculator;
+
+public class Calculation {
+    public static void calculator(int path, double a, double b) {
+        System.out.println("Your answer is :");
+        switch(path){
+            case 1 : System.out.println(a+b);
+            break;
+            case 2 : System.out.println(a-b);
+            break;
+            case 3 : System.out.println(a*b);
+            break;
+            case 4 : System.out.println(a/b);
+            break;
+            case 5 : if (b==0) {
+                System.out.println("Invalid Denominator");
+            } else {
+                System.out.println(a%b);
+            }
+            break;
+            default: System.out.println("Invalid Calculation");
+        }
+    }
+}
